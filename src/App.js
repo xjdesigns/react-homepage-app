4afa7b6b58e3122f8d4991a71fd22f9103613d2d
Jason Jacobson <jdesigns.jason@gmail.com>
2018-03-31 14:49:30 -0500

    added in electron, packager is working, can run on web or electron even though I advise against web

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import React, { Component } from 'react';
-import StuffList from './stuffList';
+import React, { Component } from 'react'
+import StuffList from './stuffList'
 import MainComp from './components/main.comp'
+import Button from './components/button/button.comp'
+
+import isElectron from 'is-electron'
+
+// import {window.ipcRenderer} from 'electron'
+// let isTrue = false
+// let window.ipcRenderer
+// if(window.require) {
+//   isTrue = true;
+//   const electron = window.require('electron');
+//   const fs = electron.remote.require('fs');
+//   ipcRenderer  = electron.ipcRenderer;
+// }
+
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
       isActive: false
     }
+
+    if(isElectron()) {
+      // setTimeout(() => {
+      //   ipcRenderer.send('hello-world', [{name: 'jasonRocks'}])
+      // }, 2000);
+      window.ipcRenderer.on('hello-world', (e) => {
+        console.warn('WHY NO WORJING', e);
+      })
+    }
   }
   componentWillMount() {
     // this.props.store.subscribe(() => {
     //   let e = this.props.store.getState().stuff.ui;
     // });
+    if(isElectron()) {
+      console.warn('comp will mount isTRUE');
+      window.ipcRenderer.on('pong', (ev, arg) => {
+        console.warn('from the main', arg);
+      })
+    }
   }
 
   clickIt = () => {
     const currentState = this.state.isActive;
     this.setState({isActive: !currentState })
+  }
+
+  fromMyChild(arg) {
+    console.warn('I AM NOTIFIED', arg);
+    if(isElectron()) {
+      window.ipcRenderer.send('hello-world', [{name: 'jasonRocks'}])
+    }
   }
 
   render() {
@@ -25,6 +62,7 @@ class App extends Component {
           <StuffList store={this.props.store} />
           <MainComp></MainComp>
           <div className={this.state.isActive ? 'hello' : 'world'}>
+            <Button cls="spx-btn spx-btn--pr" callbackParent={this.fromMyChild} theStore={this.state.isActive}></Button>
             <button className="spx-btn spx-btn--pr" onClick={this.clickIt}>Hello World</button>
           </div>
         </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+let wrapper;
+
+describe('app component', () => {
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  });
+
+  it('should check state of the comp', () => {
+    expect(wrapper.state().isActive).toBe(false);
+  });
+
+  it('should test the onClick method', () => {
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state().isActive).toBe(true)
+    // var comp = new App();
+    //
+    // comp.clickIt();
+    // expect(comp.state.isActive).toBe(true);
+  });
 });
